43c68e0718de1a37eb774471aac3bd6965bf4c80
Павел Нелюбов <pavelnelubov@gmail.com>
2020-12-07 14:02:18 +0300

    Добавьте файлы проекта.

---
+++ DigDes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace DigDes
+{
+    
+    public abstract class PrintDigDes
+    {
+        public PrintDigDes() { }
+        public void Print()
+        {
+            Console.WriteLine("DigDes");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type TypePrintDigDes = typeof(PrintDigDes);
+            ConstructorInfo magicConstructor = TypePrintDigDes.GetConstructor(Type.EmptyTypes);//get Constructor
+
+            Type TypeRuntimeMethodHandle = typeof(RuntimeMethodHandle);
+            MethodInfo magicMethod = TypeRuntimeMethodHandle.GetMethod("InvokeMethod", BindingFlags.Static | BindingFlags.NonPublic);//get InvokeMethod from RunTimeMethodHandle
+
+            PropertyInfo sigInfo = magicConstructor.GetType().GetProperty("Signature", BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance);//get signature
+            PrintDigDes abstrObj = (PrintDigDes)magicMethod.Invoke(null, new object[] { null, null, sigInfo.GetValue(magicConstructor), true , null});
+
+            abstrObj.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DigDes/astract.cs
@@ -0,0 +1,6 @@
+﻿namespace DigDes
+{
+    internal class astract
+    {
+    }
+}
